461ced0b4e14f8b23e7befad165a0233796f1bdc
iggodinho <igorgt30@gmail.com>
2022-03-15 08:41:55 -0300

    Login made

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Routes from './src/Routes/index.js';
+import AppLoading from 'expo-app-loading';
+import { useFonts, BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
+import { Ubuntu_500Medium,Ubuntu_400Regular } from '@expo-google-fonts/ubuntu';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    BebasNeue_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar backgroundColor={'black'}/>
+      <Routes/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Utils/SafeArea/index.js
@@ -0,0 +1,17 @@
+
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+
+export default function SafeArea({children}) {
+    return (
+      <SafeAreaView style={styles.container}>
+        {children}
+      </SafeAreaView>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  }
+});
